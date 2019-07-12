52511563085a23c2d9982ebe5e4ea8bde32caa57
Ryan Santos <ryansantos86@gmail.com>
2019-07-12 16:05:59 -0700

    Updates

---
+++ precache-manifest.83d1671aff808d20d26768513266d3c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c262c1e5d7f35f0140db68025f76c95",
+    "url": "/responsive_billiards_player_list/index.html"
+  },
+  {
+    "revision": "2e77e6c6ae5488c5c825",
+    "url": "/responsive_billiards_player_list/static/css/main.5da86457.chunk.css"
+  },
+  {
+    "revision": "571529ca1f69ce6891ec",
+    "url": "/responsive_billiards_player_list/static/js/2.5820c06d.chunk.js"
+  },
+  {
+    "revision": "2e77e6c6ae5488c5c825",
+    "url": "/responsive_billiards_player_list/static/js/main.96a91196.chunk.js"
+  },
+  {
+    "revision": "8ddf82becb0c44311442",
+    "url": "/responsive_billiards_player_list/static/js/runtime~main.7a039b56.js"
+  }
+]);
